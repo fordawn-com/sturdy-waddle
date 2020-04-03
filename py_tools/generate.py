dd30b23caab69ede78e62fb81ca8fbfaf51cea1d
ins <csiris@126.com>
2020-04-03 22:35:29 +0800

    wip.

--- py_tools/generate.py
+++ py_tools/generate.py
@@ -159,6 +159,3 @@ def make_targz():
 
 if __name__ == '__main__':
     app.run()
-————————————————
-版权声明：本文为CSDN博主「不会飞的小龙人」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/jilo88/java/article/details/81355201
