a958cb3ed9779755c912ae8b1e07201c634068c0
zzy <fyzzy1943@gmail.com>
2020-04-13 19:54:40 +0800

    wip,

---
+++ py_tools/spark/spark1.py
@@ -0,0 +1,12 @@
+from pyspark import SparkContext
+spark = SparkSession\
+.builder\
+.appName("My_Spark")\
+.master("local[*]")\
+.config("spark.cassandra.connection.host", "10.11.12.14,10.11.12.20,10.11.12.23")\
+.enableHiveSupport()\
+.getOrCreate()
+
+dfFormCassandra = spark.read
+      .format("org.apache.spark.sql.cassandra")
+      .option("keyspace", "aarontest").option("table", "c_staff").load()
