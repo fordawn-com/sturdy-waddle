6f22b3c1516b403dc0945ed30bba1246bb34e195
fyzzy1943 <fyzzy1943@gmail.com>
2020-04-26 09:32:31 +0800

    any question

--- spark/src/main/java/com/fordawn/spark/c8/Handler.java
+++ spark/src/main/java/com/fordawn/spark/c8/Handler.java
@@ -1,18 +1,44 @@
 package com.fordawn.spark.c8;
 
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.datastax.spark.connector.cql.CassandraConnector;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.time.Instant;
+
+@Slf4j
 public class Handler {
 
     public static final String APP_NAME = "test";
     public static final String MASTER = "local[*]";
-    public static final String HOST = "192.168.0.14";
+    //    public static final String HOST = "192.168.0.14";
+    public static final String HOST = "127.0.0.1";
 
     public static void main(String[] args) {
         SparkSession spark = SparkSession
                 .builder()
-                .appName(APP_NAME)
-                .config("spark.cassandra.connection.host",HOST)
+                .appName("tb")
+                .config("spark.cassandra.connection.host", "127.0.0.1")
+                .config("spark.cassandra.connection.port", 9045)
                 .master(MASTER)
                 .getOrCreate();
+
+        long now = Instant.now().toEpochMilli();
+
+        Dataset<Row> load = spark.read()
+                .format("org.apache.spark.sql.cassandra")
+                .option("keyspace", "thingsboard")
+                .option("table", "ts_kv_cf")
+                .load();
+        Dataset<Row> where = load.where("entity_type = 'DEVICE' and ts > 1585670400000");
+        where.show();
+
+        long count = where.count();
+        System.out.println("ts count: " + count);
     }
 
 }
